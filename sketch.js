36e80860b089fa887f95abcdddadb08146cc7101
cacadan <00001122077282sp@al.educacao.sp.gov.br>
2024-08-15 10:28:31 -0300

    atualizando

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
     createCanvas(600, 600);
     background("black");
-    }
-    function draw() {
+  }
+  
+  function draw() {
     stroke("blue");
-    fill("red"); 
-
-    If(mouseIsPressed) {
-    rect(mouseX, mouseY, 20, 35);
-    }
-
-    }
+    fill("red")
+    
+    if(mouseIsPressed){
+    rect(mouseX, mouseY, 28, 35);
+  }
+  }
 
